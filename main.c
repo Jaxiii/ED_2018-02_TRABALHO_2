e29e0030ee63fd1cd4d7f26eeb97a1d818d36367
Bruno Sanguinetti <brunoebarros@gmail.com>
2018-11-06 23:51:17 -0200

    first commit

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int input;
+
+    do
+    {
+        printf("\nMenu:\n");
+        printf("\n[1] Exame Chunin\n");
+        printf("[2] Sair\n");
+        printf("\n-> ");
+        scanf("%i", &input);
+
+        switch (input)
+        {
+            case 1:
+                break;
+            case 2:
+                printf("%s", "Finalizando...\n");
+                break;
+            default:
+                printf("%s", "Opcao invalida! Tente novamente\n");
+                break;
+        }
+    } while (input != 2);
+}
+
